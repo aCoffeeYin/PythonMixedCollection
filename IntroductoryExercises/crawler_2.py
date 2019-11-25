41459d0579e7edc8bcb3917176ba1e85adbb0b58
60km-per-h <18226922732@163.com>
2019-11-25 16:29:43 +0800

    IP地址归属地的自动查询

---
+++ IntroductoryExercises/crawler_2.py
@@ -0,0 +1,12 @@
+﻿#IP地址归属地的自动查询
+
+import requests
+url = "http://m.ip138.com/ip.asp?ip="
+try:
+    r = requests.get(url + '117.136.107.231')
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    print(r.text[-500:])
+except:
+    print("爬取失败")
+    
